e9e49762eba8a8b69ddc0a9a23e26de2ac89da27
mohsenc <mohsenc@umich.edu>
2021-10-28 16:39:39 -0400

    atom commit

---
+++ AddIntegers.java
@@ -0,0 +1,13 @@
+public class AddIntegers {
+
+    public static void main(String[] args) {
+
+        int first = 15;
+        int second = 25;
+
+        System.out.println("Enter two numbers: " + first + " " + second);
+        int sum = second + first;
+
+        System.out.println("The sum is: " + sum);
+    }
+}
